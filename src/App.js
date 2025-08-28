cfc6ad16a061ec18e33c36b919c149c02ccbf0f2
ABDUL FADIGA <tardzenyuyofficial@gmail.com>
2025-08-28 14:10:19 +0100

    adjusted the routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+
+
 import Homepage from './pages/Homepage';
 import Services from "./pages/Services";
 
 
 
-
-function App(){
-  return(
+function App() {
+  return (
     <div>
-    {/* <Homepage /> */}
-    <Services />
-     
-    
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
     </div>
-  )
-  
+  );
 }
-
-
 
 export default App;
